89ca1132a5ec55ef6c942b2c46692b8984970c03
Nirobia McKinney <mckinneynirobia@gmail.com>
2023-06-14 00:49:03 -0400

    second commit

--- index.js
+++ index.js
@@ -1,55 +1,88 @@
-//#2Define a function, as a function expression, maxOfThreethat takes three numbers as arguments 
-//and returns the largest of them. Again, the Math.max method is not allowed.
-function maxOfThree(a,b,c){
-    if(a>b && a>c){
-        return a;
-    }
-else if (b>a && b>c){
-    return b;
-}
-else{
-    return c;
-}
-}
-console.log(maxOfThree(2,5,1));
-console.log("maximumofthreenumbers+");
+//Creating Classes-give the class at least 3 properties,give the class at least 3 methods
+ //create two instances of the class (two cats)log both instances to see their properties
+//invoke each method from both instances
 
-//#3Define a function, as a function declaration, 
-//isCharAVowelthat takes a character as an argument and returns true if it is a vowel, false otherwise.
-function isCharAVowel(char) {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    return vowels.includes(char);
-  }
-  
-  console.log(isCharAVowel('a')); 
+class cat {
 
-  //#4 Define a function, as a function expression, sumArraythat takes
-  // an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]);would return 11.
-const sumArray = function(array) {
-    let sum = 0;
-    for (let num of array) {
-      sum += num;
-    }
-    return sum;
+  constructor(color, age, name) {
+    this.color = color;
+    this.age = age;
+    this.name = name;
   }
-  
-  console.log(sumArray([2, 4, 5])); 
-  //Define a function, as a function declaration, multiplyArraythat takes an 
-  //array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
-  function multiplyArray(array) {
-    let product = 1;
-    for (let num of array) {
-      product *= num;
-    }
-    return product;
-  }
-  
-  console.log(multiplyArray([2, 4, 5]));
 
-  //Define a function,
-  // as a function expression, numArgsthat returns the number of arguments passed to the function when called
-  const numArgs = function() {
-    return arguments.length;
+    introduce() {
+      console.log( `Hello, I am ${this.name}`);
+    }
+
+  sleep() {
+    console.log(`${this.name} is sleeping`);
   }
-  
-  console.log(numArgs(1,2,3)); 
+
+  cuddle() {
+    console.log(`${this.name} is enjoying some warm cuddles`);
+  }
+}
+
+// instances
+const cat1 = new cat("orange", 7, "Garfield");
+const cat2 = new cat("grey", 5, "Tinker");
+
+// properties of the instances
+console.log(cat1);
+console.log(cat2);
+
+// methods
+cat1.introduce();
+cat1.sleep();
+cat1.cuddle();
+
+cat2.introduce();
+cat2.sleep();
+cat2.cuddle();
+
+
+
+class Pirate {
+  constructor(name, age, boat) {
+    this.name = name;
+    this.age = age;
+    this.boat = boat;
+  }
+
+  introduce() {
+    console.log(`Arr! I am ${this.name}, an original pirate.`);
+  }
+
+  sailing() {
+    console.log(`${this.name} is sailing with ${this.boat}!`);
+  }
+
+  dance() {
+    console.log(`${this.name} is enjoying a dance with friends`);
+  }
+}
+
+// two arrays of pirates
+const jollyRoger = [
+  new Pirate("Captain Robi", 35, "sailboat"),
+  new Pirate("Mike Jack", 36, "yacht"),
+  new Pirate("Joe Jane", 37, "cabin cruiser")
+];
+
+const blackPearl = [
+  new Pirate("Captain Litia", 25, "tugboat"),
+  new Pirate("one eye guy", 26, "houseboat"),
+  new Pirate("one arm girl", 27, "jetboat"),
+];
+
+// Loop over the jollyRoger array 
+console.log("Pirates on the Jolly Roger:");
+jollyRoger.forEach(pirate => {
+  console.log(`Name: ${pirate.name}, Age: ${pirate.age}, Boat: ${pirate.boat}`);
+});
+
+// Loop over the blackPearl array 
+console.log("Pirates on the Black Pearl:");
+blackPearl.forEach(pirate => {
+  console.log(`Name: ${pirate.name}, Age: ${pirate.age}, Boat: ${pirate.boat}`);
+});
